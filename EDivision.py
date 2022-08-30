fdd01bdc67b0e8a1a49ca8d41f8c7f39468fb2d1
Vaibhav vats <76244950+vaibhavvatsbhartiya@users.noreply.github.com>
2022-08-30 18:28:03 +0530

    Create EDivision.py

---
+++ EDivision.py
@@ -0,0 +1,13 @@
+# question link --: https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true 
+
+# First we take input from user 
+a = int((input()))
+b = int((input()))
+
+# exclipit typeconversion 
+intdivision = int(a/b)
+flodivision = float(a/b)
+
+# now print values only 
+print(intdivision)
+print(flodivision)
